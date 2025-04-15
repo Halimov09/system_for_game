3e412cb9bcea518ca44e436c97d2eaf9b4d0bf31
halimov09 <sunnatullohhalimov@gmail.com>
2025-04-15 11:43:06 +0500

    Code-Rub

--- src/components/toolbar.jsx
+++ src/components/toolbar.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
 import { LinkMenu } from './ui'
 
-const Toolbar = ({handleToggle}) => {
+const Toolbar = ({onLinkClick}) => {
   return (
         <div className='toolbar'>
             <h2 className='h2_toolbar'>Asosiy</h2>
             <div className='toolbar_btns'>
-                <LinkMenu name="Bo'limlar" onClick={handleToggle}/>
-                <LinkMenu name="Honalar" onClick={handleToggle}/>
-                <LinkMenu name="Ombor" onClick={handleToggle}/>
-                <LinkMenu name="Hisobotlar" onClick={handleToggle}/>
+                <LinkMenu name="Bo'limlar" onClick={onLinkClick}/>
+                <LinkMenu name="Honalar" onClick={onLinkClick}/>
+                <LinkMenu name="Ombor" onClick={onLinkClick}/>
+                <LinkMenu name="Hisobotlar" onClick={onLinkClick}/>
             </div>
         </div>
   )
