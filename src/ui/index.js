9f9bac32663f916c5938cf614313e53cee11d002
halimov09 <sunnatullohhalimov@gmail.com>
2025-04-22 14:31:21 +0500

    Loader Added

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,2 +1,3 @@
 export {default as Input} from './input';
 export {default as LinkMenu} from './linkmenu';
+export {default as Loader} from './loader';
---
+++ src/ui/loader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ".././style.css"; // CSS faylni import qilamiz
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default Loader;
